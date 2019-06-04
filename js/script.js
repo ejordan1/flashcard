3bb90f26028613dee3da1701b11e0f4e0a04f040
Emerson Jordan and saswati Patra <saswati@>
2019-06-04 12:46:03 -0700

    initialize a html file call flashcard which will toggle from fron and back using js, css and bootstrape

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// $(document).ready(function() {
+//   $(".flashFront").click(function(){
+//     $(".flashFront").toggle();
+//     $(".flashBack").toggle();
+//   });
+//
+//   $(".flashBack").click(function(){
+//     $(".flashFront").toggle();
+//     $(".flashBack").toggle();
+//   });
+// });
+
+
+
+
+$(document).ready(function() {
+  $(".flashFront1").click(function(){
+    $("body").removeClass();
+    $(".flashFront1").toggle();
+    $(".flashBack1").toggle();
+  });
+
+  $(".flashBack1").click(function(){
+    $("body").removeClass();
+    $(".flashFront1").toggle();
+    $(".flashBack1").toggle();
+  });
+
+  $(".flashFront2").click(function(){
+    $("body").removeClass();
+    $(".flashFront2").toggle();
+    $(".flashBack2").toggle();
+  });
+
+  $(".flashBack2").click(function(){
+    $("body").removeClass();
+    $(".flashFront2").toggle();
+    $(".flashBack2").toggle();
+  });
+
+  $(".flashFront3").click(function(){
+    $("body").removeClass();
+    $(".flashFront3").toggle();
+    $(".flashBack3").toggle();
+  });
+
+  $(".flashBack3").click(function(){
+    $("body").removeClass();
+    $(".flashFront3").toggle();
+    $(".flashBack3").toggle();
+  });
+
+});
